9bce33dc040985219d781e29b11be549c45349d1
mosheurpau <mosheur.pau@gamil.com>
2022-03-18 03:06:07 +0600

    done

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,6 +1,7 @@
 // rsc 
 import React, { useEffect, useState } from 'react';
 import Country from '../Country/Country';
+import './Countries.css'
 
 const Countries = () => {
     const [countries, setCountries] = useState([]);
@@ -14,15 +15,18 @@ const Countries = () => {
     return (
         <div>
             <h1>Hello from countries. {countries.length}</h1>
+            <div className='countries-container'>
             {
                 countries.map(country => <Country 
                     country={country}
+                    key={country.cca3}
                     // name={country.name.common}
                     // population={country.population}
                     // region={country.region}
                     // area={country.area}
                     ></Country>)
             }
+            </div>
         </div>
     );
 };
--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -2,14 +2,16 @@ import React from 'react';
 import './Country.css'
 
 const Country = (props) => {
-    console.log(props.country);
-    const {area, region, population, name} = props.country;
+    // console.log(props.country);
+    const {area, region, population, name, flags} = props.country;
     return (
         <div className='country'>
             <h2>Country Name: {name.common}</h2>
+            <img src={flags.png} alt="" />
             <h3>Population: {population}</h3>
             <p>Region: {region}</p>
             <p>Area: {area}</p>
+
         </div>
     );
 };
