faeb0faa3b6a10d6b5f93b588a0bd3bb14ce4611
mosheurpau <mosheur.pau@gamil.com>
2022-03-18 15:55:52 +0600

    added footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 
 import Countries from './components/Countries/Countries';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       {/* <LoadCountries></LoadCountries> */}
       <Countries></Countries>
+      <Footer></Footer>
     </div>
   );
 }
